ef05f1f535f787075e6185c70b9736f70543d55e
Reed Kraft-Murphy <reed@reedmurphy.net>
2015-07-20 13:30:21 +1000

    Spec out X-True-Cache-Key header handling

--- spec/functional/matchers_spec.rb
+++ spec/functional/matchers_spec.rb
@@ -3,18 +3,23 @@ require 'spec_helper'
 describe 'have_cp_code_set' do
   before(:each) do
     stub_headers('/correct', 'x-cache-key' => 'cp-code')
+    stub_headers('/correct-true-cache-key', 'x-true-cache-key' => 'cp-code')
     stub_headers('/no-cp', {})
   end
 
-  it 'should succeed when cp code set' do
+  it 'should succeed when cp code set in x-cache-key' do
     expect(DOMAIN + '/correct').to have_cp_code('cp-code')
+  end
+
+  it 'should succeed when cp code set in x-true-cache-key' do
+    expect(DOMAIN + '/correct-true-cache-key').to have_cp_code('cp-code')
   end
 
   it 'should fail when cp code is wrong' do
     expect { expect(DOMAIN + '/correct').to have_cp_code('wrong') }.to raise_error(RuntimeError)
   end
 
-  it 'should fail x-cache-key header is not set' do
+  it 'should fail when both cache-key headers are not set' do
     expect { expect(DOMAIN + '/no-cp').to have_cp_code('wrong') }.to raise_error(RuntimeError)
   end
 end
@@ -22,14 +27,20 @@ end
 describe 'be_served_from_origin' do
   before(:each) do
     x_cache = { 'x-cache-key' => 'A/B/1234/123456/000/originsite.example.com/' }
+    x_true_cache = { 'x-true-cache-key' => 'A/B/1234/123456/000/originsite.example.com/' }
     stub_headers('/correct', x_cache)
+    stub_headers('/correct-true', x_true_cache)
     stub_request(:any, DOMAIN + '/redirect').to_return(
       body: 'abc', headers: x_cache,
       status: [300, 'message'])
   end
 
-  it 'should succeed with 200 and correct origin' do
+  it 'should succeed with 200 and correct origin in x-cache-key' do
     expect(DOMAIN + '/correct').to be_served_from_origin('originsite.example.com')
+  end
+
+  it 'should succeed with 200 and correct origin in x-true-cache-key' do
+    expect(DOMAIN + '/correct-true').to be_served_from_origin('originsite.example.com')
   end
 
   it 'should fail on 300 and correct origin' do
