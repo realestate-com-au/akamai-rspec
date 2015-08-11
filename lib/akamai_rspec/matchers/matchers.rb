57f630de749ce2a7a9c3c16f5a7bb60604987418
Bianca Gibson <bianca.gibson@rea-group.com>
2015-08-11 19:13:41 +1000

    Make separate file for x_cache headers related matchers

--- lib/akamai_rspec/matchers/matchers.rb
+++ lib/akamai_rspec/matchers/matchers.rb
@@ -4,54 +4,8 @@ require_relative 'redirects'
 require_relative 'caching'
 require_relative 'non_akamai'
 require_relative 'honour_origin_headers'
+require_relative 'x_cache_headers'
 include AkamaiHeaders
-
-X_CACHE_HEADERS = [:x_true_cache_key, :x_cache_key]
-
-RSpec::Matchers.define :be_served_from_origin do |contents|
-  match do |url|
-    match_fn = lambda { |header, expected| header && header =~ /\/#{expected}\// }
-    expect(url).to have_matching_x_cache_headers(contents, match_fn)
-  end
-end
-
-RSpec::Matchers.define :have_cp_code do |contents|
-  match do |url|
-    match_fn = lambda { |header, expected| header.include?(expected) }
-    expect(url).to have_matching_x_cache_headers(contents, match_fn)
-  end
-end
-
-RSpec::Matchers.define :have_matching_x_cache_headers do |contents, match_fn|
-  match do |url|
-    response = RestClient::Request.responsify url
-    has_x_cache_headers(response)
-    return true if x_cache_headers_match(response, contents, match_fn)
-    missing_x_cache_error(response, contents)
-    expect(response).to be_successful
-  end
-end
-
-def x_cache_headers_match(response, contents, match_fn)
-  X_CACHE_HEADERS.each do |key|
-    return true if response.headers[key] && match_fn.call(response.headers[key], contents)
-  end
-  false
-end
-
-def has_x_cache_headers(response)
-  unless X_CACHE_HEADERS.inject(false) { |bool, header| bool || response.headers.include?(header) }
-    fail "Response does not contain the debug headers"
-  end
-end
-
-def missing_x_cache_error(response, contents)
-  X_CACHE_HEADERS.each do |key|
-    if (response.headers[key])
-      fail("#{key} has value '#{response.headers[key]}' which doesn't match '#{contents}'")
-    end
-  end
-end
 
 RSpec::Matchers.define :be_forwarded_to_index do |channel|
   match do |url|
---
+++ spec/functional/x_cache_headers_spec.rb
@@ -0,0 +1,60 @@
+require 'spec_helper'
+
+describe 'have_cp_code_set' do
+  before(:each) do
+    stub_headers('/correct', 'x-cache-key' => 'cp-code')
+    stub_headers('/correct-true-cache-key', 'x-true-cache-key' => 'cp-code')
+    stub_headers('/no-cp', {})
+  end
+
+  it 'should succeed when cp code set in x-cache-key' do
+    expect(DOMAIN + '/correct').to have_cp_code('cp-code')
+  end
+
+  it 'should succeed when cp code set in x-true-cache-key' do
+    expect(DOMAIN + '/correct-true-cache-key').to have_cp_code('cp-code')
+  end
+
+  it 'should fail when cp code is wrong' do
+    expect { expect(DOMAIN + '/correct').to have_cp_code('wrong') }.to raise_error(RuntimeError)
+  end
+
+  it 'should fail when both cache-key headers are not set' do
+    expect { expect(DOMAIN + '/no-cp').to have_cp_code('wrong') }.to raise_error(RuntimeError)
+  end
+end
+
+describe 'be_served_from_origin' do
+  before(:each) do
+    x_cache = { 'x-cache-key' => 'A/B/1234/123456/000/originsite.example.com/' }
+    x_true_cache = { 'x-true-cache-key' => 'A/B/1234/123456/000/originsite.example.com/' }
+    stub_headers('/correct', x_cache)
+    stub_headers('/correct-true', x_true_cache)
+    stub_request(:any, DOMAIN + '/redirect').to_return(
+      body: 'abc', headers: x_cache,
+      status: [300, 'message'])
+  end
+
+  it 'should succeed with 200 and correct origin in x-cache-key' do
+    expect(DOMAIN + '/correct').to be_served_from_origin('originsite.example.com')
+  end
+
+  it 'should succeed with 200 and correct origin in x-true-cache-key' do
+    expect(DOMAIN + '/correct-true').to be_served_from_origin('originsite.example.com')
+  end
+
+  it 'should fail on 300 and correct origin' do
+    expect { expect(DOMAIN + '/redirect').to be_served_from_origin('originsite.example.com') }
+      .to raise_error(RuntimeError)
+  end
+
+  it 'should fail on 200 and incorrect origin' do
+    expect { expect(DOMAIN + '/correct').to be_served_from_origin('someothersite.example.com') }
+      .to raise_error(RuntimeError)
+  end
+
+  it 'should fail on 200 and origin that only partially matches' do
+    expect { expect(DOMAIN + '/correct').to be_served_from_origin('site.example.com') }
+      .to raise_error(RuntimeError)
+  end
+end
