9c7b790ae16fe67495743800ed7778e1228a2e9a
Bianca Gibson <bianca.gibson@rea-group.com>
2015-08-11 18:57:38 +1000

    Remove duplication in x_cache header related matchers

--- lib/akamai_rspec/matchers/matchers.rb
+++ lib/akamai_rspec/matchers/matchers.rb
@@ -10,44 +10,39 @@ X_CACHE_HEADERS = [:x_true_cache_key, :x_cache_key]
 
 RSpec::Matchers.define :be_served_from_origin do |contents|
   match do |url|
-    response = RestClient::Request.responsify url
-    return true if x_cache_headers_from_origin(response, contents)
-    missing_x_cache_error(response, contents)
-    expect(response).to be_successful
-    has_x_cache_headers(response)
+    match_fn = lambda { |header, expected| header && header =~ /\/#{expected}\// }
+    expect(url).to have_matching_x_cache_headers(contents, match_fn)
   end
 end
 
 RSpec::Matchers.define :have_cp_code do |contents|
   match do |url|
+    match_fn = lambda { |header, expected| header.include?(expected) }
+    expect(url).to have_matching_x_cache_headers(contents, match_fn)
+  end
+end
+
+RSpec::Matchers.define :have_matching_x_cache_headers do |contents, match_fn|
+  match do |url|
     response = RestClient::Request.responsify url
     has_x_cache_headers(response)
-    return true if x_cache_headers_contain(response, contents)
+    return true if x_cache_headers_match(response, contents, match_fn)
     missing_x_cache_error(response, contents)
     expect(response).to be_successful
   end
+end
+
+def x_cache_headers_match(response, contents, match_fn)
+  X_CACHE_HEADERS.each do |key|
+    return true if response.headers[key] && match_fn.call(response.headers[key], contents)
+  end
+  false
 end
 
 def has_x_cache_headers(response)
   unless X_CACHE_HEADERS.inject(false) { |bool, header| bool || response.headers.include?(header) }
     fail "Response does not contain the debug headers"
   end
-end
-
-def x_cache_headers_contain(response, contents)
-  X_CACHE_HEADERS.each do |key|
-    return true if response.headers[key] && response.headers[key].include?(contents)
-  end
-  false
-end
-
-
-def x_cache_headers_from_origin(response, contents)
-  X_CACHE_HEADERS.each do |key|
-    header = response.headers[key]
-    return true if (header && header =~ /\/#{contents}\//)
-  end
-  return false
 end
 
 def missing_x_cache_error(response, contents)
