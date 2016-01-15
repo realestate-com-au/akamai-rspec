59e07ab0409f515f25bb1b04810469410155bac9
Patrick Robinson <patrick.robinson@envato.com>
2016-01-15 11:53:07 +1100

    Don't check if the header exists twice
    Simplifying and removing abstractions reduces code count and complexity
    significantly.

--- lib/akamai_rspec/matchers/x_cache_headers.rb
+++ lib/akamai_rspec/matchers/x_cache_headers.rb
@@ -4,36 +4,16 @@ module AkamaiRSpec
   module Helpers
     X_CACHE_HEADERS = [:x_true_cache_key, :x_cache_key]
 
-    def have_matching_x_cache_headers(url, contents, match_fn)
-      response = RestClient::Request.responsify url
-      has_x_cache_headers(response)
-      return false unless x_cache_headers_match(response, contents, match_fn)
-      response.code == 200
-    end
-
-    def x_cache_headers_match(response, contents, match_fn)
-      X_CACHE_HEADERS.each do |key|
-        return true if response.headers[key] && match_fn.call(response.headers[key], contents)
-      end
-      false
-    end
-
-    def has_x_cache_headers(response)
-      response.headers.keys.any? { |h| X_CACHE_HEADERS.include?(h) }
-    end
-
     def x_cache_headers
       X_CACHE_HEADERS
     end
   end
 end
 
-
 RSpec::Matchers.define :be_served_from_origin do |contents|
   include AkamaiRSpec::Helpers
   match do |url|
     response = RestClient::Request.responsify url
-    has_x_cache_headers(response)
     response.headers.any? { |key, value| x_cache_headers.include?(key) && value =~ /\/#{contents}\// } && \
       response.code == 200
   end
@@ -43,7 +23,6 @@ RSpec::Matchers.define :have_cp_code do |contents|
   include AkamaiRSpec::Helpers
   match do |url|
     response = RestClient::Request.responsify url
-    has_x_cache_headers(response)
     response.headers.any? { |key, value| x_cache_headers.include?(key) && value == contents } && \
       response.code == 200
   end
