0738e6a7284ad3470e976e66b01ee7252511b325
Daniel Heath <daniel@heath.cc>
2016-10-24 16:49:49 +1100

    Make be_cacheable more reliable by checking multiple times

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -13,3 +13,16 @@ expect('google.com').to redirect_http_to_https.then be_successful
 
 This enables more fine-grained checks but also means you'll need to revise your tests when you update.
 
+## Caching
+
+We had been checking the `X-Check-Cacheable` header.
+As Akamai no longer recommends relying on this header, we now make
+a sequence of requests and look for `X-Cache: TCP_HIT`.
+
+Previously requests which returned 'not modified' from the backend
+would pass `be_cacheable`; you now need to use `be_cacheable(allow_refresh: true)`.
+
+## Origin cache headers
+
+Now actually contacts the upstream to determine cache headers
+
--- lib/akamai_rspec/matchers/caching.rb
+++ lib/akamai_rspec/matchers/caching.rb
@@ -1,18 +1,43 @@
 require 'rspec'
 require 'securerandom'
 require 'akamai_rspec/akamai_headers'
+require 'akamai_rspec/request'
 
-RSpec::Matchers.define :be_cacheable do |opts={request_count: 10}|
+RSpec::Matchers.define :be_cacheable do |request_count: 4, headers: {}, allow_refresh: false|
   match do |url|
-    responses = [1..opts[:request_count]].map { AkamaiRSpec::Request.get_with_debug_headers url }
+    @responses = [1..request_count].map {
+      AkamaiRSpec::Request.new.get url, AkamaiHeaders.akamai_debug_headers.merge(headers)
+    }
 
-    responses.any? do |response|
+    @responses.any? do |response|
       fail("Error fetching #{url}: #{response}") if response.code != 200
-      (
-        (response.headers[:x_cache] =~ /TCP(\w+)?_HIT/) and not
-        (response.headers[:x_cache] =~ /TCP_REFRESH/)
-      )
+      return allow_refresh if refresh_hit? response
+      hit?(response)
     end
+  end
+
+  def refresh_hit?(response)
+    response.headers[:x_cache] =~ /TCP_REFRESH/
+  end
+
+  def hit?(response)
+    response.headers[:x_cache] =~ /TCP(\w+)?_HIT/
+  end
+
+  def suggest_allow_refresh?(allow_refresh)
+    !allow_refresh &&
+    !@responses.any?(&method(:hit?)) &&
+    @responses.any?(&method(:refresh_hit?))
+  end
+
+  description do
+    msg = "to be cacheable (got #{cache_headers} from #{@responses.length} requests)"
+    msg += ". Try setting 'allow_refresh: true'." if suggest_allow_refresh?(allow_refresh)
+    msg
+  end
+
+  def cache_headers
+    @responses.map {|response| response.headers[:x_cache] }
   end
 end
 
